8ace9b507aae5f27bcf9667389891d1bbfbebcb3
krbrennan <krb.brennan@gmail.com>
2018-07-26 12:56:08 -0400

    Added two extra draw styles and working buttons to select which

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('button');
+let style1 = false;
+let style2 = false;
+let style3 = false;
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -12,25 +17,73 @@ let isDrawing = false;
 let initX = 0;
 let initY = 0;
 
-function draw(e) {
-  // console.log(e)
+let lastX = 0;
+let lastY = 0;
 
-  if(!isDrawing){ return }
-
-    ctx.beginPath();
-    // ctx.moveTo(initX, initY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.strokeStyle = `hsl(${color}, 50%, 50%)`;
-    ctx.lineWidth = 50;
-    ctx.stroke();
-    color ++;
-    [lastX, lastY] = e.offsetX, e.offsetY;
+function drawStyle1(e){
+  ctx.beginPath();
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle = `hsl(${color}, 50%, 50%)`;
+  ctx.lineWidth = 50;
+  ctx.stroke();
+  color ++;
 }
 
+function drawStyle2(e){
+  // "Reeds"
+  ctx.beginPath();
+  ctx.moveTo(initX, initY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle = `hsl(${color}, 50%, 50%)`;
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  color ++;
+}
+
+
+function drawStyle3(e){
+  // "FreeForm Reeds"
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle = `hsl(${color}, 50%, 50%)`;
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  color ++;
+}
+
+function draw(e){
+  if(!isDrawing){return}
+  if(style1 == true){
+    drawStyle1(e);
+  } else if(style2 == true){
+    drawStyle2(e);
+  } else if(style3 == true){
+    drawStyle3(e) }
+}
+
+function setStyle(e){
+  // When user clicks on style, set style flag to true and others to false
+  if(e.target.name == 'button1'){
+    [style1, style2, style3] = [true, false, false]
+    ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+  } else if(e.target.name == 'button2'){
+    [style1, style2, style3] = [false, true, false]
+    ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+  } else if(e.target.name == 'button3'){
+    [style1, style2, style3] = [false, false, true]
+    ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+  }
+}
+
+buttons.forEach((button) => {
+  addEventListener('click', setStyle)
+});
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
-})
-canvas.addEventListener('mouseup', () => { isDrawing = false })
-canvas.addEventListener('mouseout', () => { isDrawing = false })
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
